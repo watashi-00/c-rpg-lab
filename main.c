622be6f68b4a3b34964baee4cf6d109e87d5a3c9
watashi-00 <94692745+watashi-00@users.noreply.github.com>
2026-09-13 14:07:50 -0300

    feat: add Entity and Player structs with createPlayer factory

--- main.c
+++ main.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+typedef struct Entity {
+    int life;
+    int damage;
+} Entity;
+
+typedef struct Player {
+    char *name;
+    Entity *entity;
+
+} Player;
+
+
+Player *createPlayer(char *name) {
+    Player *player = malloc(sizeof(Player));
+
+    if (!player) {
+        return NULL;
+    }
+
+    *player = (Player) {
+        .name = name,
+        .entity = NULL,
+    };
+
+    return player;
+}
 int main(void) {
     printf("Hello, World!\n");
     return 0;
