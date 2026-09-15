10157ad62727952a2fd23f0db4051966a9dfbc9c
watashi-00 <94692745+watashi-00@users.noreply.github.com>
2026-09-15 10:24:09 -0300

    fix(main.c): limit name input length and correctly manage player state

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "includes/entity.h"
 
@@ -13,23 +14,30 @@ void changeName() {
     char player_name[8];
 
     printf("Enter player name: ");
-    scanf("%s", player_name);
+    scanf("%7s", player_name);
+
+    if (player_defined) {
+        char temp_name[8];
+        strcpy(temp_name, rPlayer.name);
+
+        strcpy(rPlayer.name, player_name);
+        printf("Changed player name: %s -> %s\n", temp_name, player_name);
+        return;
+    }
 
     Player *pPlayer = createPlayer(player_name);
     rPlayer = *pPlayer;
+    free(pPlayer);
+    player_defined = true;
 
     printf("Player name: %s\n", rPlayer.name);
 
-    free(pPlayer);
-    player_defined = true;
 }
 
 int main(void) {
 
     while (true) { // core loop
-        if (!player_defined) {
-            changeName();
-        }
+        changeName();
 
     }
     return 0;
